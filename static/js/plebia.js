ee19b3c69bd872a71e1f6eb9f29c230af3e63b46
Xavier Antoviaque <xavier@antoviaque.org>
2011-07-31 16:37:37 +0000

    Fixed multiple issues with feed refresh when multiple videos were loading at the same time: now one refresh at a time, and the right element is updated

--- static/js/plebia.js
+++ static/js/plebia.js
@@ -1,21 +1,24 @@
 
 function update() {
-    // Do this synchronously to avoid iterating before the updates have been retreived
-    //$.ajaxSetup({async:false});
-    // Iter over the posts in progress to update the progress bar every 3 seconds
-    $('.stream .stream_post_in_progress').each(function() {
-        element = this;
-        var id = $(element).attr('id').substring(5);
+    // Count the elements to invoke the the next update only after the each() loop is finished
+    var elems = $('.stream .stream_post_in_progress');
+    var count = elems.length;
+
+    // Iter over the posts in progress to update the progress bar every x seconds
+    elems.each(function() {
+        var id = $(this).attr('id').substring(5);
         $.getJSON("/ajax/post_detail/"+id+"/", function(data) {
-            torrent_progress = Math.round(data[0].fields.torrent_progress*100)/100;
-            torrent_status = data[0].fields.torrent_status;
+            var id = data[0].pk;
+            var element = '#post_'+id;
+            var torrent_progress = Math.round(data[0].fields.torrent_progress*100)/100;
+            var torrent_status = data[0].fields.torrent_status;
 
             // If download is over, play the video after 5 seconds
-            if(torrent_status == 'Completed') {
+            if(torrent_status == 'Completed' || torrent_status == 'Transcoding') {
                 $(element).removeClass('stream_post_in_progress');
                 setTimeout(function() { // Give time for the transcoding to take some advance
-                    $('.post_content', element).load('/ajax/video/'+id+'/', function() {
-                        video = $("#post_"+id+"_video");
+                    $('.post_content', $(element)).load('/ajax/video/'+id+'/', function() {
+                        var video = $("#post_"+id+"_video");
                         video.VideoJS({
                             controlsBelow: false, // Display control bar below video instead of in front of
                             controlsHiding: true, // Hide controls when mouse is not over the video
@@ -26,7 +29,7 @@ function update() {
 
                         // Play it if no other video has been started on the page
                         $('video').each(function() {
-                            video_curr_time = $(this)[0].player.currentTime();
+                            var video_curr_time = $(this)[0].player.currentTime();
                             if(video_curr_time > 0.0) {
                                 $('.stream').addClass('no_auto_play');
                             }
@@ -38,19 +41,21 @@ function update() {
                 }, 5000);
             } else { // Otherwise update the progress bar
                 if(torrent_progress > 99) {
-                    $('.torrent_status', element).html('Preparing to stream...');
-                    $('.torrent_progress', element).css('display', 'none');
+                    $('.torrent_status', $(element)).html('Preparing to stream...');
+                    $('.torrent_progress', $(element)).css('display', 'none');
                 } else if(torrent_progress > 1) {
-                    $('.torrent_status', element).html('Downloading the video...');
-                    $('.torrent_progress', element).css('display', 'inline');
+                    $('.torrent_status', $(element)).html('Downloading the video...');
+                    $('.torrent_progress', $(element)).css('display', 'inline');
                 }
                     
-                $('.torrent_progress_percent', element).html(torrent_progress);
-                $('.torrent_progress_bar', element).progressbar('option', 'value', Math.round(torrent_progress));
+                $('.torrent_progress_percent', $(element)).html(torrent_progress);
+                $('.torrent_progress_bar', $(element)).progressbar('option', 'value', Math.round(torrent_progress));
             }
         });
+
+        // Call update() again x seconds after the last each()
+        if(!--count) setTimeout("update()", 2000);
     });
-    setTimeout("update()", 3000);
 }
 
 $(function() {
